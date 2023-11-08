007a8f14f4ba59a531d4a8afd60d908f37496026
mitho <mitho@mithonk>
2023-11-08 12:54:27 +0530

    Day3

---
+++ src/Java03/Java03.java
@@ -0,0 +1,10 @@
+package Java03;
+
+public class Java03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Name is Mithursan Kalanantharasan");
+	}
+
+}
