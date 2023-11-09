f29eef743b93965a1946fe09abbdd5d7f5c92f03
mitho <mitho@mithonk>
2023-11-09 18:09:57 +0530

    day4

--- src/Java03/Java03.java
+++ src/Java03/Java03.java
@@ -1,10 +1,27 @@
 package Java03;
 
-public class Java03 {
+public class Java03{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("My Name is Mithursan Kalanantharasan");
-	}
 
+	int aValue = 442;
+	short aShort = 23;
+	long aLong =  84304;
+	double aDouble =3.4553;
+	float aFloat = 5.234f;
+
+	char aChar = 'm';
+	boolean aBoolean = true;
+	byte aByte = 127;
+
+	System.out.println(aValue);
+	System.out.println(aShort);
+	System.out.println(aLong);
+	System.out.println(aDouble);
+	System.out.println(aFloat);
+	System.out.println(aChar);
+	System.out.println(aBoolean);
+	System.out.println(aByte);
+
+	}
 }
