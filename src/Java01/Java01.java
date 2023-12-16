374d7d2b6d789c2355b680733e21fd309179af00
mithonk <mitho9798@gmail.com>
2023-12-16 08:56:27 +0530

    2

--- src/Java01/Java01.java
+++ src/Java01/Java01.java
@@ -4,6 +4,11 @@ public class Java01 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		
+
 	}
 
 }
